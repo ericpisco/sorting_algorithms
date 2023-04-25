96b7a4044f7085fc6e390972eac5c6723b40386e
Hakizimana Eric Pisco <hakizericpisco@gmail.com>
2023-04-25 09:50:35 -0700

    Betty error corrected

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -1,70 +1,68 @@
 #include "sort.h"
 
 /**
- * quick_sort_hoare - sorts an array with the Quicksort algorithm
- * @array: array of ints to sort
- * @size: size of the array
- */
-void quick_sort_hoare(int *array, size_t size)
-{
-	if (size < 2)
-		return;
-
-	quick_recursion(array, 0, (int)size - 1, size);
-}
-
-/**
- * quick_recursion - helper function for Quicksort
+ * partition_h - array partition
  * @array: array to sort
- * @left: index of the left element
- * @right: index of the right element
- * @size: size of the array
+ * @first: first position
+ * @last: last position
+ * @size: array size
+ * Return: int pivot index
  */
-void quick_recursion(int *array, int left, int right, size_t size)
+int partition_h(int *array, int first, int last, size_t size)
 {
-	int piv;
+	int pivot = array[last], i = first - 1, j = last + 1, aux;
 
-	if (left < right)
-	{
-		piv = partition(array, left, right, size);
-		quick_recursion(array, left, piv - 1, size);
-		quick_recursion(array, piv, right, size);
-	}
-}
-
-/**
- * partition - gives a piv index for Quicksort
- * @array: array to find the piv in
- * @left: index of the left element
- * @right: index of the right element
- * @size: size of the array
- *
- * Return: the index of the piv element
- */
-int partition(int *array, int left, int right, size_t size)
-{
-	int tmp, pivot = array[right];
-	size_t i, j;
-
-	i = left - 1;
-	j = right + 1;
 	while (1)
 	{
 		do {
 			i++;
 		} while (array[i] < pivot);
+
 		do {
 			j--;
 		} while (array[j] > pivot);
-		if (i >= j)
-			return (i);
-		if (i != j)
+
+		if (j < i)
+			return (j);
+		if (array[i] > array[j])
 		{
-			tmp = array[i];
+			aux = array[i];
 			array[i] = array[j];
-			array[j] = tmp;
+			array[j] = aux;
 			print_array(array, size);
 		}
 	}
-	return (0);
+}
+
+/**
+ * qsh - sorts an array of integers recursively
+ * @array: array to sort
+ * @first: first position
+ * @last: last position
+ * @size: array size
+ */
+void qsh(int *array, int first, int last, size_t size)
+{
+	int pivot;
+
+	if (first < last)
+	{
+		pivot = partition_h(array, first, last, size);
+		qsh(array, first, pivot, size);
+		qsh(array, pivot + 1, last, size);
+	}
+}
+
+/**
+ * quick_sort_hoare - sorts an array of integers using the Quick
+ * sort hoare algorithm  in ascending order
+ * @array: array to sort
+ * @size: array size
+ */
+void quick_sort_hoare(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+
+	qsh(array, 0, size - 1, size);
 }
